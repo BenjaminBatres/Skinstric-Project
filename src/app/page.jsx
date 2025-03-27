63bbe62eafe451f12e1f53ec9b6aa6e0ee8b5225
Ben <benbatres1212@gmail.com>
2025-03-27 15:57:01 -0700

    fixed para z-index

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -244,7 +244,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="absolute top-[75%] sm:top-[87%]">
+      <div className="absolute top-[75%] sm:top-[87%] z-101">
       <p
         className="pl-8 w-75 uppercase mt-18 sm:mt-0 text-sm md:text-sm md:w-90"
         id="fade-up"
