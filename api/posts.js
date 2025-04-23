6afbfc72723dd09097500492add7b08321e05d2a
Ben <benbatres1212@gmail.com>
2025-04-23 08:12:53 -0700

    testing backend p3

--- api/posts.js
+++ api/posts.js
@@ -1,26 +1,25 @@
 // api/posts.js
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
     const { name, location } = req.body;
 
     if (!name || !location) {
-      return res.status(400).json({ message: "Missing fields" });
+      return res.status(400).json({ message: 'Missing fields' });
     }
 
     try {
       const user = await prisma.user.create({
         data: { name, location },
       });
-
-      return res.status(200).json({ message: "Saved User", id: user.id });
+      return res.status(200).json({ message: 'Saved User', id: user.id });
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ message: "Server error" });
+      return res.status(500).json({ message: 'Server error' });
     }
   } else {
-    res.status(405).json({ message: "Method Not Allowed" });
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
-}
+};
