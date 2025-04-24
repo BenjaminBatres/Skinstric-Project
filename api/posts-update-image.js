4a92ef5c6cfd3b5c80b5c6fb92a6a21f296b2db8
Ben <benbatres1212@gmail.com>
2025-04-24 08:59:35 -0700

    testing database p6

--- api/posts-update-image.js
+++ api/posts-update-image.js
@@ -4,6 +4,14 @@ const prisma = new PrismaClient();
 // Image put request
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   if (req.method === "PUT") {
     const { id, image } = req.body;
 
--- api/posts.js
+++ api/posts.js
@@ -3,6 +3,14 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   if (req.method === "POST") {
     const { name, location } = req.body;
 
